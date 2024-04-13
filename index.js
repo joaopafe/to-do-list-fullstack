2991ae04237bbdabb136ace6d3540df17281511d
João Paulo <joao.ea2020@gmail.com>
2024-04-13 17:49:12 -0300

    add the GET /task endpoint

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ message: `Hello World` }));
 
+app.get("/task", (req, res) => {
+  res.json({
+    tasks: [{ title: "Fazer compras" }, { title: "Consertar o pc" }],
+  });
+});
+
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
